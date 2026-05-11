10860f749faba6a889cb8eebb2bca907d3f794e1
Jose-ant-1 <jgonsol1510@g.educaand.es>
2026-05-11 14:55:03 +0200

    el token ahora dura más

--- src/main/java/com/example/proyecto/config/JwtTokenProvider.java
+++ src/main/java/com/example/proyecto/config/JwtTokenProvider.java
@@ -15,7 +15,7 @@ public class JwtTokenProvider {
 // Esta cadena debe ser muy larga (mínimo 64 caracteres) para cumplir con el algoritmo HS512 que usas
     private static final String SECRET_SEED = "esta_es_una_clave_secreta_muy_larga_y_segura_para_el_proyecto_carrito_2026_spring_boot";
     private final Key key = Keys.hmacShaKeyFor(SECRET_SEED.getBytes(StandardCharsets.UTF_8));
-    private final long jwtExpirationInMs = 3600000; // 1 hora
+    private final long jwtExpirationInMs = 36000000; // 10 hora
 
     public String generarToken(String email) {
         Date ahora = new Date();
